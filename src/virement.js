377a5f347b86349a79b3025605cfba34bc00ea3f
yassinenaciri <yassinen84@gmail.com>
2022-01-12 08:45:38 +0100

    yassine

--- src/virement.js
+++ src/virement.js
@@ -1,84 +1,107 @@
 import React from 'react';
 
+export default class Virement extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      montant: '',
+      motif: '',
+      NoComptebeneficiare: '',
+      NoCompteEmetteur: '',
+    };
+  }
 
-
-export default class Virement extends React.Component { 
-
-    constructor(props)
-    {
-        super(props);
-        this.state = { 
-            montant: '',
-            motif :'',
-            NoComptebeneficiare :'',
-            NoCompteEmetteur:'',
-        };
+  submit = () => {
+    if (this.validateCompteBeneficiare() && this.validateCompteEmetteur() && this.validateMontant() && this.validateMotif()) {
+      this.sendRequest();
     }
+  };
 
-    submit=()=>{
-        if(this.validateCompteBeneficiare() && this.validateCompteEmetteur() && this.validateMontant() && this.validateMotif()){
-            this.sendRequest();
-        }
+  sendRequest = () => {};
 
-}
+  validateCompteBeneficiare = () => {
+    return this.validateCompte(this.state.NoComptebeneficiare);
+  };
 
- sendRequest =()=>{
-  console.log('eaz');
-}  
-
-validateCompteBeneficiare =()=>{
- return this.validateCompte(this.state.NoComptebeneficiare); 
-}
-validateCompteEmetteur =()=>{
+  validateCompteEmetteur = () => {
     return this.validateCompte(this.state.NoCompteEmetteur);
-   }
-validateCompte =(compte )=>{
-    console.log(compte.length);
-    if (compte.length ==16 ){
-        return true;
-    }
-    return false;
-}
+  };
 
-validateMontant =( )=>{
-    let montant =parseInt(this.state.montant);
-    if (montant>=10 && montant<10000){
-        return true;
+  validateCompte = compte => {
+    if (compte === null) {
+      return true;
+    }
+    if (compte.length === 16) {
+      return true;
     }
     return false;
-}
-validateMotif =()=>{
-    if (this.state.motif.length >0){
-        return true
+  };
+
+  validateMontant = () => {
+    let montant = parseInt(this.state.montant);
+
+    if (this.state.montant === null) {
+      return true;
+    }
+    if (montant >= 10 && montant < 10000) {
+      return true;
     }
     return false;
-}
+  };
+  validateMotif = () => {
+    if (this.state.motif === null || this.state.motif.length > 0) {
+      return true;
+    }
+    return false;
+  };
 
   render() {
     return (
-        <div>
+      <div>
+        <label>motif</label>
+        <input
+          name="motif"
+          id="motif"
+          onChange={event => {
+            this.setState({ ...this.state, motif: event.target.value });
+          }}
+        />
+        {this.validateMotif() ? null : <p id="errorMotif">le motif ne peut pas etre vide</p>}
 
-            <input name='motif' id='motif' onChange={(event)=>{this.setState({...this.state,motif:event.target.value})}} />
-            {this.validateMotif() ? null : <p id='errorMotif'>le motif ne peut pas etre vide</p> }
+        <label>montant</label>
+        <input
+          name="montant"
+          id="montant"
+          onChange={event => {
+            this.setState({ ...this.state, montant: event.target.value });
+          }}
+        />
+        {this.validateMontant() ? null : <p id="errorMontant">entrez un montant superieur à 10 et inferieur à 10000</p>}
 
+        <label>compte beneficiaire</label>
+        <input
+          name="NoComptebeneficiare"
+          id="NoComptebeneficiare"
+          onChange={event => {
+            this.setState({ ...this.state, NoComptebeneficiare: event.target.value });
+          }}
+        />
+        {this.validateCompteBeneficiare() ? null : <p id="errorNoComptebeneficiare">entrez un compte beneficiaire valide</p>}
 
-            <input name='montant'  id='montant' onChange={(event)=>{this.setState({...this.state,montant:event.target.value})}}/>
-           {this.validateMontant()? null : <p id='errorMontant'>entrez un montant superieur à 10 et inferieur à 1000</p> }
+        <label>compte emetteur</label>
+        <input
+          name="NoCompteEmetteur"
+          id="NoCompteEmetteur"
+          onChange={event => {
+            this.setState({ ...this.state, NoCompteEmetteur: event.target.value });
+          }}
+        />
+        {this.validateCompteEmetteur() ? null : <p id="errorNoCompteEmetteur">entrez un compte emetteur valide</p>}
 
-
-            <input name='NoComptebeneficiare' id='NoComptebeneficiare' onChange={(event)=>{this.setState({...this.state,NoComptebeneficiare:event.target.value})}}/>
-            {this.validateCompteBeneficiare() ? null : <p id='errorNoComptebeneficiare'>entrez un compte beneficiaire valide</p> }
-
-
-            <input name='NoCompteEmetteur' id='NoCompteEmetteur' onChange={(event)=>{this.setState({...this.state,NoCompteEmetteur:event.target.value})}} />
-            {this.validateCompteEmetteur()? null : <p id='errorNoCompteEmetteur'>entrez un compte emetteur valide</p> }
-
-
-            <button type='submit' id='executerVirement' onClick={this.submit} >executer le virement</button>
-        </div>
-
+        <button type="submit" id="executerVirement" onClick={this.submit}>
+          executer le virement
+        </button>
+      </div>
     );
   }
-
-
 }
--- src/virement.test.js
+++ src/virement.test.js
@@ -1,166 +1,149 @@
-import React from "react";
-import Enzyme , {shallow,configure} from 'enzyme';
+import React from 'react';
+import Enzyme, { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Virement from "./virement";
+import Virement from './virement';
 import ReactTestUtils from 'react-dom/test-utils';
-import {jest} from '@jest/globals';
+import { jest } from '@jest/globals';
 
+Enzyme.configure({ adapter: new Adapter() });
 
-Enzyme.configure({adapter : new Adapter()});
+describe('VirementTest', () => {
+  it('elements should mount', () => {
+    const app = shallow(<Virement />);
+    app.find('#executerVirement');
+    app.find('#montant');
+    app.find('#NoComptebeneficiare');
+    app.find('#motif');
+    app.find('#NoCompteEmetteur');
+  }),
+    it('should send request ', () => {
+      const app = shallow(<Virement />);
+      const submitBtn = app.find('#executerVirement');
+      const montantInput = app.find('#montant');
+      const motifInput = app.find('#motif');
+      const NoComptebeneficiareInput = app.find('#NoComptebeneficiare');
+      const NoCompteEmetteurInput = app.find('#NoCompteEmetteur');
+      montantInput.simulate('change', {
+        target: {
+          name: 'montant',
+          value: '10',
+        },
+      });
+      motifInput.simulate('change', {
+        target: {
+          name: 'motif',
+          value: 'mon motif valide',
+        },
+      });
+      NoComptebeneficiareInput.simulate('change', {
+        target: {
+          name: 'NoComptebeneficiare',
+          value: '11111111111111',
+        },
+      });
+      NoCompteEmetteurInput.simulate('change', {
+        target: {
+          name: 'NoCompteEmetteur',
+          value: '2222222222222222',
+        },
+      });
 
-describe('VirementTest',()=>{
-
-    it("elements should mount",()=>{
-        const app =shallow( < Virement />);
-        app.find('#executerVirement');
-        app.find('#montant');
-        app.find('#NoComptebeneficiare');
-        app.find('#motif');
-        app.find('#NoCompteEmetteur');
-
-    },
-    it('should send request ',()=>{
-        const app =shallow( < Virement />);
-        const submitBtn = app.find('#executerVirement');
-        const montantInput = app.find('#montant');
-        const motifInput = app.find('#motif');
-        const NoComptebeneficiareInput = app.find('#NoComptebeneficiare');
-        const NoCompteEmetteurInput = app.find('#NoCompteEmetteur');
-        montantInput.simulate('change', {
-            target: {
-                name: 'montant',
-                value:'10'
-            },
-          });
-          motifInput.simulate('change', {
-            target: {
-                name: 'motif',
-                value:'mon motif valide'
-            },
-          });
-          NoComptebeneficiareInput.simulate('change', {
-            target: {
-                name: 'NoComptebeneficiare',
-                value:'1111111111111111'
-            },
-          });
-          NoCompteEmetteurInput.simulate('change', {
-            target: {
-                name: 'NoCompteEmetteur',
-                value:'2222222222222222'
-            },
-          });
-
-        const instance = app.instance()
-        const spy = jest.spyOn(instance, 'sendRequest')
-        submitBtn.simulate('click');
-        expect(spy).toHaveBeenCalled();
-    })
-    ,
-
-
-
-    it('compte beneficiaire input test ',()=>{
-        const app =shallow( < Virement />);
-        const submitBtn = app.find('#executerVirement');
-        const NoComptebeneficiareInput = app.find('#NoComptebeneficiare');
-       
-        NoComptebeneficiareInput.simulate('change', {
-            target: {
-                name: 'NoComptebeneficiare',
-                value:'111111111'
-            },
-          });
-        
-         expect(app.find('#errorNoComptebeneficiare').length).toEqual(1);
-        const errorMontant = app.find('#errorNoComptebeneficiare');
-        expect(errorMontant.text()).toBe('entrez un compte beneficiaire valide')
-
-        NoComptebeneficiareInput.simulate('change', {
-            target: {
-                name: 'NoComptebeneficiare',
-                value:'1111111111111111'
-            },
-          });
-        expect(app.find('#errorNoComptebeneficiare').length).toEqual(0);
+      const instance = app.instance();
+      const spy = jest.spyOn(instance, 'sendRequest');
+      submitBtn.simulate('click');
+      expect(spy).toHaveBeenCalled();
     }),
+    it('compte beneficiaire input test ', () => {
+      const app = shallow(<Virement />);
+      const submitBtn = app.find('#executerVirement');
+      const NoComptebeneficiareInput = app.find('#NoComptebeneficiare');
 
-    
-    it('compte emetteur input test ',()=>{
-        const app =shallow( < Virement />);
-        const submitBtn = app.find('#executerVirement');
-        const NoCompteEmetteurInput = app.find('#NoCompteEmetteur');
-       
-        NoCompteEmetteurInput.simulate('change', {
-            target: {
-                name: 'NoCompteEmetteur',
-                value:'111111111'
-            },
+      NoComptebeneficiareInput.simulate('change', {
+        target: {
+          name: 'NoComptebeneficiare',
+          value: '111111111',
+        },
+      });
+      expect(app.find('#errorNoComptebeneficiare').length).toEqual(1);
+      const errorMontant = app.find('#errorNoComptebeneficiare');
+      expect(errorMontant.text()).toBe('entrez un compte beneficiaire valide');
+
+      NoComptebeneficiareInput.simulate('change', {
+        target: {
+          name: 'NoComptebeneficiare',
+          value: '1111111111111111',
+        },
+      });
+      expect(app.find('#errorNoComptebeneficiare').length).toEqual(0);
+    }),
+    it('compte emetteur input test ', () => {
+      const app = shallow(<Virement />);
+      const submitBtn = app.find('#executerVirement');
+      const NoCompteEmetteurInput = app.find('#NoCompteEmetteur');
+
+      NoCompteEmetteurInput.simulate('change', {
+        target: {
+          name: 'NoCompteEmetteur',
+          value: '111111111',
+        },
+      });
+
+      expect(app.find('#errorNoCompteEmetteur').length).toEqual(1);
+      const errorMontant = app.find('#errorNoCompteEmetteur');
+      expect(errorMontant.text()).toBe('entrez un compte emetteur valide');
+
+      NoCompteEmetteurInput.simulate('change', {
+        target: {
+          name: 'NoCompteEmetteur',
+          value: '1111111111111111',
+        },
+      });
+      expect(app.find('#errorNoCompteEmetteur').length).toEqual(0);
+    }),
+    it('motif input test ', () => {
+      const app = shallow(<Virement />);
+      const submitBtn = app.find('#executerVirement');
+      const montantInput = app.find('#motif');
+
+      montantInput.simulate('change', {
+        target: {
+          name: 'motif',
+          value: '',
+        },
+      });
+      expect(app.find('#errorMontant').length).toEqual(1);
+      const errorMontant = app.find('#errorMotif');
+      expect(errorMontant.text()).toBe('le motif ne peut pas etre vide');
+
+      montantInput.simulate('change', {
+        target: {
+          name: 'motif',
+          value: 'monMotif',
+        },
+      });
+      expect(app.find('#errorMotif').length).toEqual(0);
+    }),
+    it('montant input test ', () => {
+      const app = shallow(<Virement />);
+      const submitBtn = app.find('#executerVirement');
+      const montantInput = app.find('#montant');
+
+      montantInput.simulate('change', {
+        target: {
+          name: 'montant',
+          value: '9',
+        },
+      });
+      expect(app.find('#errorMontant').length).toEqual(1);
+      const errorMontant = app.find('#errorMontant');
+      expect(errorMontant.text()).toBe('entrez un montant superieur à 10 et inferieur à 10000');
+
+      montantInput.simulate('change', {
+        target: {
+          name: 'montant',
+          value: '10',
+        },
+      });
+      expect(app.find('#errorMontant').length).toEqual(0);
     });
-        
-    expect(app.find('#errorNoCompteEmetteur').length).toEqual(1);
-    const errorMontant = app.find('#errorNoCompteEmetteur');
-    expect(errorMontant.text()).toBe('entrez un compte emetteur valide')
-
-        NoCompteEmetteurInput.simulate('change', {
-            target: {
-                name: 'NoCompteEmetteur',
-                value:'1111111111111111'
-            },
-          });
-        expect(app.find('#errorNoCompteEmetteur').length).toEqual(0);
-    })
-
-    ,
-    
-    
-    it('motif input test ',()=>{
-        const app =shallow( < Virement />);
-        const submitBtn = app.find('#executerVirement');
-        const montantInput = app.find('#motif');
-       
-        montantInput.simulate('change', {
-            target: {
-                name: 'motif',
-                value:''
-            },
-          });
-        expect(app.find('#errorMontant').length).toEqual(1);
-        const errorMontant = app.find('#errorMotif');
-        expect(errorMontant.text()).toBe('le motif ne peut pas etre vide')
-
-        montantInput.simulate('change', {
-            target: {
-                name: 'motif',
-                value:'monMotif'
-            },
-          });
-        expect(app.find('#errorMotif').length).toEqual(0);
-
-    }),
-
-
-    it('montant input test ',()=>{
-        const app =shallow( < Virement />);
-        const submitBtn = app.find('#executerVirement');
-        const montantInput = app.find('#montant');
-       
-        montantInput.simulate('change', {
-            target: {
-                name: 'montant',
-                value:'9'
-            },
-          });
-        expect(app.find('#errorMontant').length).toEqual(1);
-        const errorMontant = app.find('#errorMontant');
-        expect(errorMontant.text()).toBe('entrez un montant superieur à 10 et inferieur à 1000')
-
-        montantInput.simulate('change', {
-            target: {
-                name: 'montant',
-                value:'10'
-            },
-          });
-        expect(app.find('#errorMontant').length).toEqual(0);
-    }))
-})
+});
